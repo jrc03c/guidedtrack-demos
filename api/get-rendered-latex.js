3570f8ffc77893a473cb2e76241c55164a7ff9ea
jrc03c <jrc03c@pm.me>
2020-12-03 08:46:11 -0600

    ...

--- api/get-rendered-latex.js
+++ api/get-rendered-latex.js
@@ -1,7 +1,12 @@
 const katex = require("katex")
+const storedSecret = process.env.GT_SECRET
 
 module.exports = function(request, response){
   try {
+    let receivedSecret = request.headers.secret
+
+    if (receivedSecret !== storedSecret) return response.json({error: "Incorrect secret!"})
+
     let html = `
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
       ${katex.renderToString(request.query.latex)}
