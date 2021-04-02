35db27dfe9a154de17ba59525579f718f04e4d53
jrc03c <jrc03c@pm.me>
2021-04-02 16:56:20 -0500

    ...

--- api/mostly-fails.js
+++ api/mostly-fails.js
@@ -2,8 +2,8 @@ module.exports = function(request, response){
   let shouldFail = Math.random() < 0.75
 
   if (shouldFail){
-    return response.status(500).send("Failed!")
+    return response.status(500).json({message: "Failure!"})
   } else {
-    return response.status(200).send("Succeeded!")
+    return response.status(200).json({message: "Success!"})
   }
 }
