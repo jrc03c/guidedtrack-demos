46ae40ac40f9e2514a9759b48d4f0314f924f5ff
jrc03c <jrc03c@pm.me>
2023-05-06 10:01:36 -0500

    ...

--- api/wait.js
+++ api/wait.js
@@ -1,4 +1,13 @@
 module.exports = function (request, response) {
   const ms = parseInt(request.query.ms)
-  setTimeout(() => response.send(true), ms)
+
+  if (isNaN(ms)) {
+    return response
+      .status(400)
+      .send(
+        "This endpoint requires a query string parameter called 'ms' with a numerical value. To wait for 5 seconds, for example, use: https://guidedtrack-demos.vercel.app/api/wait/?ms=5000"
+      )
+  } else {
+    return setTimeout(() => response.send(true), ms)
+  }
 }
