7f50f56047fa0958740f80a5497813ea0051195a
jrc03c <jrc03c@pm.me>
2023-05-06 10:02:44 -0500

    ...

--- api/wait.js
+++ api/wait.js
@@ -5,9 +5,9 @@ module.exports = function (request, response) {
     return response
       .status(400)
       .send(
-        "This endpoint requires a query string parameter called 'ms' with a numerical value. To wait for 5 seconds, for example, use: https://guidedtrack-demos.vercel.app/api/wait/?ms=5000"
+        "ERROR: Malformed request. This endpoint requires a query string parameter called 'ms' with a numerical value. To wait for 5 seconds, for example, use: https://guidedtrack-demos.vercel.app/api/wait/?ms=5000"
       )
   } else {
-    return setTimeout(() => response.send(true), ms)
+    return setTimeout(() => response.status(200).send(), ms)
   }
 }
